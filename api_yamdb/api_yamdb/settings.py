e85d0332fdb17853cbce32acb4e8aed685e45dd7
unknown <kovalchuk.alexander@googlemail.com>
2022-03-05 21:42:58 +0300

    c36

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default="test")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.30.222']
+ALLOWED_HOSTS = ['localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
